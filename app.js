da8542edc18e5d5be971338d63782d741f7e2740
Javohir Narimonov <javohirnarimonov2009@gmail.com>
2026-02-02 17:55:11 +0500

    js 5dars

---
+++ app.js
@@ -0,0 +1,26 @@
+// String methods
+
+// 1 .toUpperCase()
+console.log("text".toUpperCase());
+// 2. toLowercase()
+console.log("String"[1]); // Index
+
+// 3. indexOf
+let str = "Widthet with id";
+
+console.log(str.indexOf('hi'));
+
+let LoremText = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit veritatis sit quas sint at maxime culpa saepe in est maiores.'
+
+// 4. includes
+console.log(LoremText.includes('eliit1223')); // boolean
+
+// 5. startsWith
+console.log(str.endsWith('Widget'));
+
+// 6. endWith
+console.log(str.endsWith('id'));
+// 7. slice()
+let text = 'Hello, world!'
+
+
